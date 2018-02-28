f94ea1e791142338a11a7f4565f8f80b89c2a2f2
SomeMissingColon <thomasnoidion@gmail.com>
2018-02-28 11:18:03 -0500

    finished donation

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
     resources :donations, only:[:new,:create,:edit,:update]
 
   end
+  get '/user/projects', to: 'users#projects', as: :user_project
+  get '/user/donations', to: 'users#donations', as: :user_donations
+  get '/search', :to => 'projects#search_results', :as => :search
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
