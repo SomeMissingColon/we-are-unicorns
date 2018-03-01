49d075d06ca2a79a41c18fc2254a415fc3c11f69
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-03-01 13:57:04 -0500

    added amount received to cards

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,20 @@ class Project < ApplicationRecord
   validates :description, presence: true
   validates :user, presence: true
   mount_uploader :photo, PhotoUploader
+
+
+  def received_donations
+    project = self
+    # authorize project
+
+    received_sum = 0
+    if project.donations.present?
+      project.donations.map do |donation|
+        if donation.status == "accept"
+          received_sum += donation.amount
+        end
+      end
+    end
+    received_sum
+  end
 end
