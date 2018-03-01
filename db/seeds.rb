8facc27a4a6264acf610787a9d90b81333293ace
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-03-01 17:40:18 -0500

    smaller seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Donation.destroy_all
-Project.destroy_all
-FocusArea.destroy_all
-User.destroy_all
+# Donation.destroy_all
+# Project.destroy_all
+# FocusArea.destroy_all
+# User.destroy_all
 
-puts "creating users"
-user1 = {email:'user1@user1.com',
-         password: '123456',
-         remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833061/Avatar-1.jpg' }
+# puts "creating users"
+# user1 = {email:'user1@user1.com',
+#          password: '123456',
+#          remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833061/Avatar-1.jpg' }
 
-user2 = {email: 'user2@user2.com',
-         password: '123456',
-         remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833061/Avatar-2.jpg' }
+# user2 = {email: 'user2@user2.com',
+#          password: '123456',
+#          remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833061/Avatar-2.jpg' }
 
 
-user1 = User.create(user1)
-user2 = User.create(user2)
+# user1 = User.create(user1)
+# user2 = User.create(user2)
 
 puts "creating focus area"
 FocusArea.create(name: "Ageing")
@@ -57,56 +57,56 @@ FocusArea.create(name: "Transportation")
 FocusArea.create(name: "Women`s Status & Issues")
 
 
-project1 = { name:'free the ducks',
-           description:'free em up',
-           user: user1,
-           focus_area_id: 1,
-           remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project1 = { name:'free the ducks',
+#            description:'free em up',
+#            user: user1,
+#            focus_area_id: 1,
+#            remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-project2 = { name:'free the rocks',
-description:'free em up',
-user: user1,
-focus_area_id: 2,
-remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project2 = { name:'free the rocks',
+# description:'free em up',
+# user: user1,
+# focus_area_id: 2,
+# remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-project3 = { name:'free the nips',
-description:'free em up',
-user: user1,
-focus_area_id: 3,
-remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project3 = { name:'free the nips',
+# description:'free em up',
+# user: user1,
+# focus_area_id: 3,
+# remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-project4 = { name:'free the tips',
-description:'free em up',
-user: user1,
-focus_area_id: 4,
-remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project4 = { name:'free the tips',
+# description:'free em up',
+# user: user1,
+# focus_area_id: 4,
+# remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-project5 = { name:'free the creeps',
-description:'free em up',
-user: user2,
-focus_area_id: 5,
-remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project5 = { name:'free the creeps',
+# description:'free em up',
+# user: user2,
+# focus_area_id: 5,
+# remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-project6 = { name:'free the flips',
-description:'free em up',
-user: user2,
-focus_area_id: 6,
-remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project6 = { name:'free the flips',
+# description:'free em up',
+# user: user2,
+# focus_area_id: 6,
+# remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-project7 = { name:'free the drips',
-description:'free em up',
-user: user2,
-focus_area_id: 7,
-remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
+# project7 = { name:'free the drips',
+# description:'free em up',
+# user: user2,
+# focus_area_id: 7,
+# remote_photo_url: 'https://res.cloudinary.com/dwikboy8/image/upload/t_media_lib_thumb/v1519833062/free-stuff.jpg' }
 
-puts "creating projects"
-project1 = Project.new(project1)
-project1.save!
-project2 = Project.create(project2)
-project3 = Project.create(project3)
-project4 = Project.create(project4)
-project5 = Project.create(project5)
-project6 = Project.create(project6)
-project7 = Project.create(project7)
+# puts "creating projects"
+# project1 = Project.new(project1)
+# project1.save!
+# project2 = Project.create(project2)
+# project3 = Project.create(project3)
+# project4 = Project.create(project4)
+# project5 = Project.create(project5)
+# project6 = Project.create(project6)
+# project7 = Project.create(project7)
 
-puts "done"
+# puts "done"
